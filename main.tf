4c12d054098f062d795850babd49868cbbc499ea
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-10-04 12:41:40 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_iam_user" "default" {
 }
 
 resource "aws_iam_user_policy" "default" {
-  count  = var.enabled && var.policy != "" ? 1 : 0  # Only create if enabled and policy is not empty
+  count  = var.enabled && var.policy_enabled && var.policy_arn != "" ? 1 : 0
   name   = var.name
   #user   = aws_iam_user.default.*.name[0]
   user       = aws_iam_user.default[count.index].name
