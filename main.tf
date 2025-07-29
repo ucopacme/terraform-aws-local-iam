a9f59c2c8a458bd397605c441885ef89b6b7ee32
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2025-07-29 13:57:58 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,24 +9,22 @@ resource "aws_iam_user" "default" {
 }
 
 resource "aws_iam_user_policy" "default" {
-  count  = var.enabled && var.policy_enabled && var.policy != "" ? 1 : 0
+  count  = var.enabled && var.policy != "" ? 1 : 0
 
   name   = var.name
-  user   = aws_iam_user.default[count.index].name
+  user   = aws_iam_user.default[0].name
   policy = var.policy
 }
 
-
 resource "aws_iam_user_policy_attachment" "default" {
-  count      = var.enabled && var.policy_arn_enabled && var.policy_arn != "" ? 1 : 0
-  #user       = aws_iam_user.default.*.name[0]
-  user       = aws_iam_user.default[count.index].name
+  count      = var.enabled && var.policy_arn != "" ? 1 : 0
+  user       = aws_iam_user.default[0].name
   policy_arn = var.policy_arn
 }
 
 resource "aws_iam_user_group_membership" "default" {
   count      = var.enabled && length(var.groups) > 0 ? 1 : 0
-  user       = aws_iam_user.default[count.index].name
+  user       = aws_iam_user.default[0].name
   groups     = var.groups
   depends_on = [aws_iam_user.default]
 }
